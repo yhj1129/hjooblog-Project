a0329bf4af490d9f33f0af652ba0f52e221a8978
HJoo <yoon3798@naver.com>
2023-04-19 21:18:23 +0900

    initial commit

---
+++ src/main/java/com/hjoo/hjooblog/HjooblogApplication.java
@@ -0,0 +1,13 @@
+package com.hjoo.hjooblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HjooblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HjooblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hjoo/hjooblog/HjooblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hjoo.hjooblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HjooblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
