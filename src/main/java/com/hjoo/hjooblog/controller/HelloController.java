b59838bb44505a9296003da16fdc89af48615994
HJoo <yoon3798@naver.com>
2023-04-19 21:54:10 +0900

    "화면 연동 완료"

---
+++ src/main/java/com/hjoo/hjooblog/controller/HelloController.java
@@ -0,0 +1,51 @@
+package com.hjoo.hjooblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/hello")
+@Controller
+public class HelloController {
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "user/joinForm";
+    }
+
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "user/loginForm";
+    }
+
+    @GetMapping("/user/updateForm")
+    public String userUpdateForm() {
+        return "user/updateForm";
+    }
+
+    @GetMapping({ "/", "/board" })
+    public String main() {
+        return "board/main";
+    }
+
+    @GetMapping("/board/{id}")
+    public String boardDetail() {
+        return "board/detail";
+    }
+
+    @GetMapping("/board/saveForm")
+    public String boardSaveForm() {
+        return "board/saveForm";
+    }
+
+    @GetMapping("/board/{id}/updateForm")
+    public String boardUpdateForm() {
+        return "board/updateForm";
+    }
+
+    @GetMapping("/user/profileUpdateForm")
+    public String profileUpdateForm(){
+        return "user/profileUpdateForm";
+    }
+
+}
