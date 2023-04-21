dfb056b9fe9db326d4e7ec29c1158065a149a6db
HJoo <yoon3798@naver.com>
2023-04-21 20:08:33 +0900

    "1단계 회원가입 뷰 완료"

--- src/main/java/com/hjoo/hjooblog/HjooblogApplication.java
+++ src/main/java/com/hjoo/hjooblog/HjooblogApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class HjooblogApplication {
 
+    @Profile("dev")
     @Bean
     CommandLineRunner init(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder){
         return args -> {
